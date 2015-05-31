28c096b756dd4ee71b16b4f02bcaa4a952021a1d
Supasorn <supasorn@gmail.com>
2015-05-31 00:36:14 -0700

    auto commit

--- web/websock.py
+++ web/websock.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit, disconnect
 import time
+import os
 
 app = Flask(__name__)
 app.debug = True
@@ -21,6 +22,22 @@ def test_message(message):
 @socketio.on('connect', namespace='/test')
 def test_connect():
     emit('my response', {'data': 'Server Connected'})
+
+
+def captureWB(wb):
+    folder = "static/capturewb"
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    os.system("cp /home/supasorn/pano/outside/021_0.jpg " + folder + "/" + wb)
+    return "/" + folder + "/" + wb
+
+@socketio.on('capturewb', namespace="/test")
+def capturewb():
+    wbs = ['sunny', 'shade']
+    for wb in wbs:
+        url = captureWB(wb)
+        emit('capturewb', {'wb': wb, 'url': url})
 
 @socketio.on('disconnect request', namespace='/test')
 def disconnect_request():
